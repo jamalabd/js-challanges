8ddfa46797eb2169474b33c7d2b801607e895cf3
jamalabd <jamalabdul47@gmail.com>
2021-10-15 18:24:22 -0400

    Completed code challange credit card mask

--- beginner/CreditCardMask.js
+++ beginner/CreditCardMask.js
@@ -1,1 +1,16 @@
-function maskify(cc) {}
+// changes all but the last four intiger int '#'
+function maskify(cc) {
+  let newArr = cc.split('');
+  let dumpArr = [];
+  let finalArray;
+  const repeat = (item, times) => {
+    dumpArr.unshift(item.pop());
+    times && --times && repeat(item, times);
+  };
+  repeat(newArr, 4);
+  newArr.forEach((element, i) => (newArr[i] = '#'));
+  finalArray = newArr.concat(dumpArr).join('');
+  return finalArray;
+}
+
+maskify('4556364607935616');
